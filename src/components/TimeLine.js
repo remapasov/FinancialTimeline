ada153f8c98673a063dcb3b89b8e9e0882d01386
remapasov <remapasov@yandex.ru>
2025-08-05 16:17:05 +0300

    Start commit

---
+++ src/components/TimeLine.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+// import { Timeline, DataSet } from 'vis-timeline/standalone'
+// import { DataSet } from "vis-data/peer"
+// import { Timeline } from "vis-timeline/peer";
+// import "../../vis-timeline/styles/vis-timeline-graph2d.css";
+import "../../node_modules/vis-timeline/dist/vis-timeline-graph2d.css";
+import Timeline from 'react-visjs-timeline'
+
+// import { traitImg, toolboxImg, cashImg } from './constants';
+
+export const TimeLineComponent = (props) => {
+
+  const container = document.getElementById('timeLineContainer');
+  const items = new DataSet([
+    {id: 1, content: 'item 1', start: '2013-04-20'},
+    {id: 2, content: 'item 2', start: '2013-04-14'},
+    {id: 3, content: 'item 3', start: '2013-04-18'},
+    {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
+    {id: 5, content: 'item 5', start: '2013-04-25'},
+    {id: 6, content: 'item 6', start: '2013-04-27'}
+  ]);
+
+  const options = {};
+
+  // const timeline = new Timeline(container, items, options);
+
+  return (
+    <div>
+      <div>?RRRRRRRRRRRRRRRRRR</div>
+      <Timeline items={items} />
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "index-bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
+          'css-loader',
+          // 'sass-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              api: "modern-compiler",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ]
+};
